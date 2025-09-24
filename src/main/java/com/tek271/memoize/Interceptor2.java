d281ba65d4e0ea314ca386b58e0a00bfe8ac8c8e
Abdul Habra <abdul.habra@gmail.com>
2025-09-23 23:51:48 -0400

    add more logging in Interceptor2

--- src/main/java/com/tek271/memoize/Interceptor2.java
+++ src/main/java/com/tek271/memoize/Interceptor2.java
@@ -4,16 +4,21 @@ import com.tek271.memoize.utils.ReflectionTools;
 import net.bytebuddy.implementation.bind.annotation.*;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class Interceptor2 {
   @RuntimeType
   public static Object intercept(@This Object self,
-                                 @Origin Method method,
+                                 @Origin Method originalMethod,
                                  @AllArguments Object[] args,
                                  @SuperMethod Method superMethod) {
     System.out.println("intercept:");
-    System.out.println("  originalMethod=" + method);
-    System.out.println("  superMethod   =" + superMethod);
+    System.out.println("  self          = " + self);
+    System.out.println("  originalMethod= " + originalMethod);
+    System.out.println("  superMethod   = " + superMethod);
+    System.out.println("  args          = " + Arrays.toString(args));
+
+
     return ReflectionTools.invokeMethod(self, superMethod, args);
   }
 
--- src/main/java/com/tek271/memoize/RememberFactory2.java
+++ src/main/java/com/tek271/memoize/RememberFactory2.java
@@ -6,7 +6,6 @@ import com.tek271.memoize.utils.Utils;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.implementation.MethodDelegation;
-import net.bytebuddy.implementation.bind.annotation.*;
 import net.bytebuddy.matcher.ElementMatchers;
 
 import java.lang.reflect.Method;
